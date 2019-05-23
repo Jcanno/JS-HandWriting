d117e941d39ffd76bc52eb2386dc09c8caafc99c
金志强 <641283231@qq.com>
2019-05-23 15:31:41 +0800

    js-instanceOf end

---
+++ js-instanceof.js
@@ -0,0 +1,18 @@
+function instanceOf(left, right) {
+  let R = right.prototype;
+  let L = left.__proto__;
+
+  while(true) {
+
+    if(L === null) {
+      return false
+    }
+
+    if(L === R) {
+      return true 
+    }else{ 
+      L = L.__proto__;
+    }
+
+  }
+}

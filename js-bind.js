768cf02b9130f38c00f4a301c30d601cdeab5a80
Jacano <641283231@qq.com>
2020-06-03 16:04:54 +0800

    fix:fix bind

--- js-bind.js
+++ js-bind.js
@@ -9,16 +9,11 @@ Function.prototype.bind = function (context, ...args) {
 		throw new Error("Function.prototype.bind - what is trying to be bound is not callable");
 	}
 	var self = this;
-	var fNOP = function () {};
-
 	var fbound = function () {
-			self.apply(this instanceof self ? 
-					this : 
-					context, args.concat(Array.prototype.slice.call(arguments)));
+		self.apply(this instanceof self ? 
+			this : 
+			context, args.concat(Array.prototype.slice.call(arguments)));
 	}
-
-	fNOP.prototype = this.prototype;
-	fbound.prototype = new fNOP();
 
 	return fbound;
 }
